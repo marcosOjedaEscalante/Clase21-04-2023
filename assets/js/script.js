1311ab4e4c2f81d25aae6ffbe9a8072e9e741b03
Marcos Ojeda Escalante <marcos.ojeda.escalante@gmail.com>
2023-04-22 10:39:48 -0400

    gráfico incorporado

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,30 @@ btnConsultar.addEventListener('click', function () {
         dataType: "json",
         success: function (response) {
             console.log(response);
+            var chart = new CanvasJS.Chart("graficoUno", {
+                theme: "dark1", // "light2", "dark1", "dark2"
+                animationEnabled: true, // change to true		
+                title: {
+                    text: "Pokemón"
+                },
+                data: [
+                    {
+                        // Change type to "bar", "area", "spline", "pie",etc.
+                        type: "column",
+                        dataPoints: [
+                            { label: "hp", y: response.stats[0].base_stat },
+                            { label: "attack", y: response.stats[1].base_stat },
+                            { label: "defense", y: response.stats[2].base_stat },
+                            { label: "special-attack", y: response.stats[3].base_stat },
+                            { label: "special-defense", y: response.stats[4].base_stat },
+                            { label: "speed", y: response.stats[5].base_stat }
+                        ]
+                    }
+                ]
+            });
+            chart.render();
         }
     });
+
     txtNombrePokemon.value = '';
 });
